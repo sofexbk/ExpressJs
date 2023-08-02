4dd33d54bbcecbc97975d729e38594a344993cc0
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-08-02 23:44:28 +0100

    mock

--- src/__test__/controllers/auth.test.js
+++ src/__test__/controllers/auth.test.js
@@ -1,4 +1,11 @@
 const {authRegisterContorller}=require('../../controllers/auth')
+const User=require('../../database/schemas/User')
+
+
+//mock
+jest.mock('../../database/schemas/User')
+
+
 
 //fake request
 const request={
@@ -7,6 +14,15 @@ const request={
         password:'fake_password',
     }
 }
-it('should send a status code of 400 when user exists',()=>{
-    authRegisterContorller(request);
+//a error occurs when we dont return a reposnse object so we should:
+const response={
+    
+}
+it('should send a status code of 400 when user exists',async()=>{
+    User.findOne.mockImplementationOnce(()=>({id:1,email:'email',password:'password'}))
+    await authRegisterContorller(request);
 })
+
+
+//we need  mocking(userfindone) for real test with real data
+//in unit tsst we dont need to call database , isolated from other pieces of application
