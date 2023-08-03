b1cf66d85f80aa5fdc8e500336831d4fecf3923c
soufianbouktaib1@gmail.com <109437519+sofexbk@users.noreply.github.com>
2023-08-03 13:33:10 +0100

    pasword is correct not password mock testt expect

--- src/__test__/controllers/auth.test.js
+++ src/__test__/controllers/auth.test.js
@@ -1,28 +1,53 @@
 const {authRegisterContorller}=require('../../controllers/auth')
 const User=require('../../database/schemas/User')
+const {hashPassword} =require('../../utils/helpers')
+
+
+jest.mock('../../utils/helpers',()=>({
+    //hashPassword:jest.fn((x)=>x)
+     hashPassword:jest.fn(()=>'hash password')
+}))
 
 
 //mock
 jest.mock('../../database/schemas/User')
 
-
-
 //fake request
 const request={
     body:{
         email:'fake_email',
-        password:'fake_password',
+        pasword:'fake_password',
     }
 }
 //a error occurs when we dont return a reposnse object so we should:
 const response={
-    
+    status:jest.fn((x)=>x),
+    send:jest.fn((x)=>x),
 }
 it('should send a status code of 400 when user exists',async()=>{
-    User.findOne.mockImplementationOnce(()=>({id:1,email:'email',password:'password'}))
-    await authRegisterContorller(request);
+    User.findOne.mockImplementationOnce(()=>({
+        id:1,
+        email:'email',
+        password:'password'
+    }))
+    await authRegisterContorller(request,response);
+    expect(response.status).toHaveBeenCalledWith(400);
+    expect(response.send).toHaveBeenCalledTimes(1);
 })
-
 
 //we need  mocking(userfindone) for real test with real data
 //in unit tsst we dont need to call database , isolated from other pieces of application
+
+
+it('should send a status code of 201 when new user is created',async()=>{
+  User.findOne.mockResolvedValueOnce(undefined) 
+  //hashPassword.mockReturnValueOnce('hash')  after i commented this i recive hash password as a console message
+  User.create.mockResolvedValueOnce({
+    id:1,
+    email:'email',
+    pasword:'password'
+  })
+  await authRegisterContorller(request,response); 
+  expect(hashPassword).toHaveBeenCalledWith('fake_password')
+    
+})
--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,13 @@
 const User=require('../database/schemas/User')
+const {hashPassword} =require('../utils/helpers')
+
 
 async function authRegisterContorller(request,response){
     const {email}=request.body;
     const userDB=await User.findOne({ email });
     if(userDB){
-     response.status(400).send({msg:'User already exists!'})
+     response.status(400)
+     response.send({msg:'User already exists!'})
     }else{
       const password=hashPassword(request.body.pasword);
       console.log(password)
@@ -15,3 +18,6 @@ async function authRegisterContorller(request,response){
 }
 
 module.exports={authRegisterContorller} 
+
+
+/// we dont want to call hashpassword because it will call the bycrypt library , but we dont want that, and that break rule of unit testing
